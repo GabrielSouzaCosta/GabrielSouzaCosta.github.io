038ad6ecca7f52e3bc7da6535993804102cbeb65
GabrielSouzaCosta <gabrielsscosta2010@hotmail.com>
2023-10-11 10:32:36 -0300

    translation and fixes

---
+++ .vercel/output/static/_astro/LanguageSwitcher.19484cae.js
@@ -0,0 +1,1 @@
+import{L as i}from"./LanguageSwitcher.f0b16fa3.js";import"./index.ddc2566a.js";import"./_commonjsHelpers.725317a4.js";import"./i18next.1d1c9b7d.js";export{i as default};
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTrigger } from './ui/sheet'
+import { t } from 'i18next'
+import LanguageSwitcher from './LanguageSwitcher'
+import ThemeToggle from './ThemeToggle.astro'
 
 const Sidebar = () => {
+
+  const [ isSidebarOpen, setIsSidebarOpen ] = useState(false);
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  }
+
   return (
-    <Sheet>
+    <Sheet
+      open={isSidebarOpen}
+      onOpenChange={setIsSidebarOpen}
+    >
       <SheetTrigger
         aria-label="Open sidebar menu"
         className="block lg:hidden text-2xl text-neutral-700 dark:text-neutral-300"
@@ -11,14 +24,43 @@ const Sidebar = () => {
         <i className="ph ph-list"></i>
       </SheetTrigger>
       <SheetContent>
-        <SheetHeader>
-          <SheetDescription asChild>
-            <div>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
+        <SheetDescription asChild>
+          <nav className="flex-1 flex flex-col items-center justify-center gap-y-8 h-full">
+            <ul className="flex flex-col text-lg uppercase justify-center items-center h-full gap-y-4 text-neutral-800 dark:text-neutral-300">
+              <li className="transition-colors hover:text-primary-600 hover:dark:text-primary-400">
+                <a href="/#about" onClick={closeSidebar}>
+                  {t("About me")}
+                </a>
+              </li>
+              <li className="transition-colors hover:text-primary-600 hover:dark:text-primary-400">
+                <a href="/#experience" onClick={closeSidebar}>
+                  {t("Experience")}
+                </a>
+              </li>
+              <li className="transition-colors hover:text-primary-600 hover:dark:text-primary-400">
+                <a href="/#projects" onClick={closeSidebar}>
+                  {t("Projects")}
+                </a>
+              </li>
+              <li className="transition-colors hover:text-primary-600 hover:dark:text-primary-400">
+                <a href="/#services" onClick={closeSidebar}>
+                  {t("Services")}
+                </a>
+              </li>
+              <li className="transition-colors hover:text-primary-600 hover:dark:text-primary-400">
+                <a href="/#contact" onClick={closeSidebar}>
+                  {t("Contact")}
+                </a>
+              </li>
+            </ul>
+            <div className="flex gap-x-4 items-center">
+              <LanguageSwitcher />
+              {/* <div>
+                <ThemeToggle />
+              </div> */}
             </div>
-          </SheetDescription>
-        </SheetHeader>
+          </nav>
+        </SheetDescription>
       </SheetContent>
     </Sheet>
   )
